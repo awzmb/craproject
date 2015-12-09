d8d01ff266cd2b70229e71fbe0a04be9ff00c87c
Dennis Bee <awzm.gaming@gmail.com>
2015-12-09 01:00:27 +0100

    Tagesende 09.12.

---
+++ analysis.R
@@ -0,0 +1,165 @@
+######################## LIBRARIES #############################
+library(parallel)
+
+
+
+######################## ZUSAMMENFUEHRUNG #############################
+#Vektor mit den Namen der resultsNameMatrix erzeugen (wichtig, sofern
+#zwischenzeitlich der Vektor kValues geaendert wurde! sonst werden
+#nicht die korrekten Namen verwendet)
+resultsNameMatrixNames <- as.numeric(colnames(resultsNameMatrix))
+
+#Erzeugen eines Vektors mit den Namen der Ergebnisse zum
+#spaeteren Zugriff
+sumResultsNameVector <- NULL
+
+#Vektoren mit Laenge = Anzahl Werte fuer k erzeugen
+AUCVector <- rep(NA, length(resultsNameMatrixNames))
+names(AUCVector) <- colnames(resultsNameMatrix)
+
+NRRVector <- rep(NA, length(resultsNameMatrixNames))
+names(NRRVector) <- colnames(resultsNameMatrix)
+
+#Zusammenfuehrung der Ergebnisse per Schleife pro Wert fuer k
+for (i in seq_len(ncol(resultsNameMatrix))) {
+  
+  #Vektor zuruecksetzen
+  sumResults <- NULL
+  
+  #Ergebnisse pro Spalte zusammenfuehren (Vektoren aneinanderhaengen)
+  for (j in seq_len(length(resultsNameMatrix[,i]))) {
+    sumResults <- c(sumResults, get(resultsNameMatrix[j,i]))
+  }
+  
+  #Ergebnis umbenennen und ablegen
+  assign(paste0("sumResults_k", resultsNameMatrixNames[i]), sumResults)
+  
+  #Hinzufuegen des Ergebnisnamens in den Vektor sumResultsNameVector
+  sumResultsNameVector <- c(sumResultsNameVector, paste0("sumResults_k", resultsNameMatrixNames[i]))
+  
+  #AUC und NRR in die Ergebnisvektoren ueberfuehren
+  AUCVector[i] <- fAUC(sumResults)
+  NRRVector[i] <- fNRR(sumResults)
+}
+
+
+
+
+########################
+# Kurven fuer alle k Werte
+########################
+#Erstellen der ROC-Kurven
+plotColors = c("sandybrown", "blue", "green", "tomato", "steelblue", "burlywood3", "magenta3", "black")
+plot(NULL, xlim=c(0,1), ylim=c(0,1), main="ROC curve for k Nearest Neighbor", ylab="True Postitive Rate", xlab="False Positive Rate")
+#Erzeugen der Linien
+for (i in seq_len(length(sumResultsNameVector))) {
+  lines(fROC(get(sumResultsNameVector[i])), col=plotColors[i])
+}
+legend(0.8, 0.9, paste0("k = ", resultsNameMatrixNames), cex=0.8, col=plotColors, lty=1)
+
+
+
+
+########################
+# Proportionalitaet AUC / NRR
+########################
+#Erstellen der ROC-Kurven
+plot(NULL, xlim=c(0,20), ylim=c(0,100), main="Proportionality NRR / AUC", ylab=" ", xlab=" ")
+lines(NRRVector, col=plotColors[1])
+lines(AUCVector, col=plotColors[2], type = "b")
+legend(0.8, 0.3, c("NRR", "AUC"), cex=0.8, col=plotColors, lty=1)
+
+as.numeric(names(AUCVector))
+
+
+#Erstellen der ROC-Kurven
+plot(NULL, xlim=c(0,1), ylim=c(0,1), main="ROC curve", ylab="True Postitive Rate", xlab="False Positive Rate")
+lines(fROC(sumResults))
+fAUC(sumResults) # in Prozent
+fNRR(sumResults) # in Prozent
+
+#for (i in 1:5) {
+#  do.call("rbind", )
+#}
+
+
+#Erstellen der ROC-Kurven
+plotColors = c("sandybrown", "blue", "green", "tomato", "steelblue", "burlywood3", "magenta3", "black")
+plot(NULL, xlim=c(0,1), ylim=c(0,1), main="ROC curve (T5)", ylab="True Postitive Rate", xlab="False Positive Rate")
+#Erzeugen der Linien
+for (j in seq_len(length(resultsNameMatrix[,i]))) {
+  sumResults <- c(sumResults, get(resultsNameMatrix[j,i]))
+}
+lines(fROC(sumResults_t5_k2), col=plotColors[1])
+lines(fROC(sumResults_t5_k3), col=plotColors[2])
+lines(fROC(sumResults_t5_k5), col=plotColors[3])
+#lines(fROC(sumResults_t5_k10), col=plotColors[4])
+#lines(fROC(sumResults_t5_k20), col=plotColors[5])
+#lines(fROC(sumResults_t5_k50), col=plotColors[6])
+#lines(fROC(sumResults_t5_k100), col=plotColors[7])
+lines(fROC(sumResults_t5_k1682), col=plotColors[8])
+legend(0.8, 0.5, c("k = 2","k = 3","k = 5","k = 10","k = 20","k = 50","k = 100", "k = MAX"), cex=0.8, col=plotColors, lty=1)
+
+
+
+
+#Ergebnisse zusammenführen
+sumResults_k2 <- c(sumResults_t1_k2, sumResults_t2_k2, sumResults_t3_k2, sumResults_t4_k2, sumResults_t5_k2)
+sumResults_k3 <- c(sumResults_t1_k3, sumResults_t2_k3, sumResults_t3_k3, sumResults_t4_k3, sumResults_t5_k3)
+sumResults_k5 <- c(sumResults_t1_k5, sumResults_t2_k5, sumResults_t3_k5, sumResults_t4_k5, sumResults_t5_k5)
+sumResults_k10 <- c(sumResults_t1_k10, sumResults_t2_k10, sumResults_t3_k10, sumResults_t4_k10, sumResults_t5_k10)
+sumResults_k20 <- c(sumResults_t1_k20, sumResults_t2_k20, sumResults_t3_k20, sumResults_t4_k20, sumResults_t5_k20)
+sumResults_k50 <- c(sumResults_t1_k50, sumResults_t2_k50, sumResults_t3_k50, sumResults_t4_k50, sumResults_t5_k50)
+sumResults_k100 <- c(sumResults_t1_k100, sumResults_t2_k100, sumResults_t3_k100, sumResults_t4_k100, sumResults_t5_k100)
+sumResults_k1682 <- c(sumResults_t1_k1682, sumResults_t2_k1682, sumResults_t3_k1682, sumResults_t4_k1682, sumResults_t5_k1682)
+
+
+#Farbgebung fuer die Plots definieren
+plotColors = c("sandybrown", "blue", "green", "tomato", "steelblue", "burlywood3", "magenta3", "black")
+
+
+#Plot ueber Ergebnisse der gesamten Analyse
+plot(NULL, xlim=c(0,1), ylim=c(0,1), main="ROC curve (T5)", ylab="True Postitive Rate", xlab="False Positive Rate")
+lines(fROC(sumResults_k2), col=plotColors[1])
+lines(fROC(sumResults_k3), col=plotColors[2])
+lines(fROC(sumResults_k5), col=plotColors[3])
+lines(fROC(sumResults_k10), col=plotColors[4])
+lines(fROC(sumResults_k20), col=plotColors[5])
+lines(fROC(sumResults_k50), col=plotColors[6])
+lines(fROC(sumResults_k100), col=plotColors[7])
+lines(fROC(sumResults_k1682), col=plotColors[8])
+legend(0.8, 0.5, c("k = 2","k = 3","k = 5","k = 10","k = 20","k = 50","k = 100", "k = MAX"), cex=0.8, col=plotColors, lty=1)
+
+
+#Uebersicht k50 ueber alle Datensaetze
+plot(NULL, xlim=c(0,1), ylim=c(0,1), main="ROC curve (comparison t1 - t5 datasets)", ylab="True Postitive Rate", xlab="False Positive Rate")
+lines(fROC(sumResults_t1_k50), col=plotColors[1])
+lines(fROC(sumResults_t2_k50), col=plotColors[2])
+lines(fROC(sumResults_t3_k50), col=plotColors[3])
+lines(fROC(sumResults_t4_k50), col=plotColors[4])
+lines(fROC(sumResults_t5_k50), col=plotColors[5])
+legend(0.8, 0.5, c("t1", "t2", "t3", "t4", "t5"), cex=0.8, col=plotColors, lty=1)
+
+
+#Werte fuer AUC und NRR fuer alle k in Matrix schreiben
+sumResults_NRR_AUC <- matrix(0, 8, 2)
+rownames(sumResults_NRR_AUC) <- c("k2", "k3", "k5", "k10", "k20", "k50", "k100", "kMAX")
+colnames(sumResults_NRR_AUC) <- c("NRR", "AUC")
+sumResults_NRR_AUC[1,] <- c(fNRR(sumResults_k2), fAUC(sumResults_k2))
+sumResults_NRR_AUC[2,] <- c(fNRR(sumResults_k3), fAUC(sumResults_k3))
+sumResults_NRR_AUC[3,] <- c(fNRR(sumResults_k5), fAUC(sumResults_k5))
+sumResults_NRR_AUC[4,] <- c(fNRR(sumResults_k10), fAUC(sumResults_k10))
+sumResults_NRR_AUC[5,] <- c(fNRR(sumResults_k20), fAUC(sumResults_k20))
+sumResults_NRR_AUC[6,] <- c(fNRR(sumResults_k50), fAUC(sumResults_k50))
+sumResults_NRR_AUC[7,] <- c(fNRR(sumResults_k100), fAUC(sumResults_k100))
+sumResults_NRR_AUC[8,] <- c(fNRR(sumResults_k1682), fAUC(sumResults_k1682))
+
+
+#
+plot(NULL, xlim=c(1,2), ylim=c(1,2), main="ROC curve (comparison t1 - t5 datasets)", ylab="NRR", xlab="AUC")
+lines(as.numeric(sumResults_NRR_AUC[1:7,1]/sumResults_NRR_AUC[1:7,2]), col=plotColors[1])
+
+#
+plot(NULL, xlim=c(0,100), ylim=c(0,100), main="NRR / AUC ratio")
+lines(sumResults_NRR_AUC[1:7,1], col=plotColors[1])
+lines(sumResults_NRR_AUC[1:7,2], col=plotColors[2])
---
+++ testuser.R
@@ -0,0 +1,104 @@
+######################## LIBRARIES #############################
+library(parallel)
+
+
+
+######################## VARIABLEN #############################
+#Wert fuer kNN (Anzahl an Neighbours)
+kValues = c(2, 3, 5, 10, 20, 50, 100)
+
+
+
+######################## AUSWERTUNG #############################
+for (kValue in c(kValues, 1682)) {
+
+    #Erzeugen des Active User Vektors (activeUser) per Aufruf eines
+  #Vektors aus der uDataActiveUser, wobei die userID aus den Zeilennamen
+  #der Matrix abgeleitet wird (z. B. userID 196 = Zeile 35 in 
+  #uDataActiveUser)
+  activeUser <- uDataActiveUser1[which(rownames(uDataActiveUser1) == userID),]
+  
+  #ItemID ermitteln die mit 5 bewertet wurde und auf NA gesetzt werden muss,
+  #dazu wird zunaechst die zweite Spalte von userMatrix an der aktuellen UserID
+  #(UserID aus DataXtab) abgerufen (Vergleich der UserID mit Spalte 1 aus
+  #userMatrix um Wert aus Spalte 2 zu erhalten)
+  activeUser[userMatrix1[which(userMatrix1[,1] == userID), 2]] <- NA
+  
+  #ERSETZT DURCH KNN
+  #Abfragen der Items aus spearmanMatrix, die in activeUser mit ueber 3 bewertet wurden 
+  #so bekommt man viele Vektoren mit den Korrelationskoeffizienten, die am Ende
+  #aufsummiert werden um activeUserSpearman zu erhalten
+  #activeUserSpearman <- colSums(spearmanMatrix[which(activeUser > 3),], na.rm = TRUE)
+  
+  ####KNN
+  #Korrelationen aller Items die im aktiven Vektor mit über 3 bewertet sind aus
+  #der Spearman-Matrix ziehen und zu einer Matrix zusammenfuegen. 
+  ColSumMatrix <- spearmanMatrix1[which(activeUser > 3),]
+  
+  #Schleife zur Berechnung der k nearest neighbours fuer den aktuellen
+  #Active User Vektor. 
+  for (j in seq_len(nrow(ColSumMatrix))) {
+    
+    #Ranked und sortiert jede Zeile der Matrix ColSumMatrix, um die
+    #1 bis kValue (oben definiert) "besten" ItemIDs zu bekommen, welche
+    #mit dem jeweiligen Item am besten korrelieren
+    kNNItems <- as.numeric(names(sort(rank(-ColSumMatrix[j,], ties.method = "random", na.last = "keep")))[1:kValue])
+    
+    #Ersetzt alle Stellen in der Matrix ColSumMatrix durch NA, die nicht
+    #in kNNItems aufgeführt sind (also alle Items, die nicht die jeweilig
+    #k Nearest-Neighbors sind). So können nun per colSums die Spalten der
+    #Matrix aufaddiert werden.
+    ColSumMatrix[j,which(!names(ColSumMatrix[j,]) %in% kNNItems)] <- NA
+  }
+  
+  ####/KNN
+  
+  #Active User Vektor mit der Summe aller Spalten der ColSumMatrix auffüllen
+  #(kNN ist damit abgeschlossen). NA-Werte werden bei diesem Zwischenschritt
+  #entfernt (Funktion nach Vorgabe J. Knoll).
+  #activeUserSpearman <- colSums(ColSumMatrix, na.rm = TRUE)
+  activeUserSpearman=apply(ColSumMatrix,2,function(v){if(any(!is.na(v))){sum(v,na.rm=T)}else{NA}})
+  
+  #Stellen in activeUserSpearman, die in activeUser mit 1-5 bewertet wurden, werden mit
+  #NA überschrieben damit die Rangliste korrekt ausgegeben wird (bereits bewertete
+  #Items würden die Rangliste verfaelschen)
+  activeUserSpearman[which(!is.na(activeUser))] <- NA
+  
+  #activeUser wird hier nur auf diejenigen Items reduziert, die auch in userMatrix aufgeführt
+  #sind (userMatrix in der Zeile UserID i und dann nur die Spalten 2:Ende). Alle Items die nicht
+  #in userMatrix fuer diese UserID erscheinen werden auf NA gesetzt, damit die Rangfolge mit den
+  #richtigen Bewertungen / Items berechnet wird.
+  activeUserSpearman[!(as.numeric(names(activeUserSpearman)) %in% userMatrix1[(userMatrix[,1] == userID),2:ncol(userMatrix1)])] <- NA
+  
+  #Erzeugen der Rangliste aus activeUserSpearman.
+  activeUserItemRanking <- as.numeric(names(sort(rank(-activeUserSpearman, na.last = "keep"))[1:10]))
+  
+  assign(paste0("testUserResults_t1_u765_k", kValue), activeUserItemRanking)
+}
+
+
+#Matrix mit Ergebnissen fuer verschiedene k erzeugen
+testUserResults_t1_u765 <- matrix(0, 8, 10)
+testUserResults_t1_u765[1,] <- testUserResults_t1_u765_k2
+testUserResults_t1_u765[2,] <- testUserResults_t1_u765_k3
+testUserResults_t1_u765[3,] <- testUserResults_t1_u765_k5
+testUserResults_t1_u765[4,] <- testUserResults_t1_u765_k10
+testUserResults_t1_u765[5,] <- testUserResults_t1_u765_k20
+testUserResults_t1_u765[6,] <- testUserResults_t1_u765_k50
+testUserResults_t1_u765[7,] <- testUserResults_t1_u765_k100
+testUserResults_t1_u765[8,] <- testUserResults_t1_u765_k1682
+rownames(testUserResults_t1_u765) <- c("k2", "k3", "k5", "k10", "k20", "k50", "k100", "kMAX")
+
+
+#Rated 5 Item fuer User 765
+as.numeric(userMatrix[which(userMatrix1[,1] == 765),2])
+
+
+#Namen in Matrix schreiben
+testUserResults_t1_u765_Names <- matrix(0, 8, 10)
+
+for (i in seq_len(ncol(testUserResults_t1_u765_Names))) {
+  testUserResults_t1_u765_Names[1,i] <- itemDescription[testUserResults_t1_u765[1,i],2]
+}
+
+itemDescription[testUserResults_t1_u765[1,1],2]
